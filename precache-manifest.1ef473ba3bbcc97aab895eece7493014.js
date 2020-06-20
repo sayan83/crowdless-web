45efcca43bede153a543f9de150ec39ee6dc4704
Sayantan <sayanbis83@gmail.com>
2020-06-21 00:27:39 +0530

    Testing v1.0
    Shows live crowd info
    Books appointment
    Shows appointment details

---
+++ precache-manifest.1ef473ba3bbcc97aab895eece7493014.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5b197021198b8d74a6a931721417b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4afdc628cdbe7d6acef",
+    "url": "/static/css/main.65f88cfd.chunk.css"
+  },
+  {
+    "revision": "d4d2bf958b54130d9621",
+    "url": "/static/js/2.8e0b1a94.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.8e0b1a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4afdc628cdbe7d6acef",
+    "url": "/static/js/main.a0d78b49.chunk.js"
+  },
+  {
+    "revision": "2be14e1ce8f5ce330575",
+    "url": "/static/js/runtime-main.8e065001.js"
+  },
+  {
+    "revision": "33a84d6022b7388ac34615349df26d26",
+    "url": "/static/media/AboutUs.33a84d60.svg"
+  },
+  {
+    "revision": "9ce940bde3478f15748ec57b61411328",
+    "url": "/static/media/RegisterYourStore.9ce940bd.svg"
+  },
+  {
+    "revision": "2f453d9463c97a3f43201fef69e52e19",
+    "url": "/static/media/contactus.2f453d94.png"
+  },
+  {
+    "revision": "15a459d0eb2fcf1d2a12ddb2ef50ca34",
+    "url": "/static/media/footcounticon.15a459d0.svg"
+  },
+  {
+    "revision": "da55f8a6f7be8fac6b0d0bd7fb5e6c07",
+    "url": "/static/media/landingscreen2.da55f8a6.svg"
+  },
+  {
+    "revision": "faf4ced46d1a55b678227a7729f0818d",
+    "url": "/static/media/liveCount.faf4ced4.svg"
+  },
+  {
+    "revision": "ce260a927f273c409de99597fdda35ad",
+    "url": "/static/media/moreCustomer.ce260a92.svg"
+  },
+  {
+    "revision": "15e09a8e1ca3b9d4fe0c8514c6f13729",
+    "url": "/static/media/moreEfficient.15e09a8e.png"
+  },
+  {
+    "revision": "7217ee21f8b6cb9f54e2bede2f8950e3",
+    "url": "/static/media/moreProfit.7217ee21.png"
+  },
+  {
+    "revision": "4eb00e9185b0ae1456e239788c2ef75a",
+    "url": "/static/media/search-icon.4eb00e91.svg"
+  },
+  {
+    "revision": "9a3e4ea7b02adcb0d6b566243a6ae808",
+    "url": "/static/media/services.9a3e4ea7.png"
+  },
+  {
+    "revision": "387e7d821f010f512ef57b7e7bac15c2",
+    "url": "/static/media/storeContact.387e7d82.svg"
+  },
+  {
+    "revision": "706cebe096e3e8539e7a3801e560e43f",
+    "url": "/static/media/storeType.706cebe0.svg"
+  }
+]);
